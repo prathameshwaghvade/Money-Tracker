b7705f135f4d1b9ec27b1c10658c11e57c46ffc3
prathamesh <luffygithub@gmail.com>
2025-03-22 13:30:15 +0530

    setup html and css structure Now tomorrow we will make this form functional. By using api,endpoint for transactions...

--- src/App.js
+++ src/App.js
@@ -2,10 +2,66 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <h1 color="red">Hello</h1>
-   <p>This is a money tracker app..</p>
-    </div>
+    <main>
+      <h1>$400 <span>.00</span></h1>
+
+      <form>
+        <div className='basic'>
+        <input type="text" placeholder={'+200 new samsung tv'}/>
+        <input type="datetime-local"/>
+        </div>
+
+        <div className='description'>
+        <input type="text" placeholder={'desciption'}/>
+        </div>
+        <button type='submit'>Add a new transaction</button>
+      </form>
+      
+      <div className='transactions'>
+        <div className='transaction'>
+
+          <div className='left'>
+            <div className='name'>New samsung tv</div>
+            <div className='description'>It was time for new tv</div>
+          </div>
+
+
+          <div className='right'>
+            <div className='price red'>-$500</div>
+            <div className='datetime'>2025-04-10 15:45</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+
+          <div className='left'>
+            <div className='name'>Gig job new website</div>
+            <div className='description'>It was time for new site</div>
+          </div>
+
+          
+          <div className='right'>
+            <div className='price green'>+$400</div>
+            <div className='datetime'>2025-04-10 15:45</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+
+          <div className='left'>
+            <div className='name'>Iphone</div>
+            <div className='description'>It was time for new ios</div>
+          </div>
+
+          
+          <div className='right'>
+            <div className='price red'>-$900</div>
+            <div className='datetime'>2025-04-10 15:45</div>
+          </div>
+        </div>
+      </div>
+    </main>
+    
   );
 }
 
